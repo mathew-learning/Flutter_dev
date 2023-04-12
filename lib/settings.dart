650b06a25ee1dd8efe96cadb411ae19e9e1a5119
SandeepKomal <sandeepkomalp@gmail.com>
2023-04-12 14:53:39 +0530

    added settings page

---
+++ lib/settings.dart
@@ -0,0 +1,16 @@
+
+ 
+
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: Text(
+            'Settings',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      );
+}
