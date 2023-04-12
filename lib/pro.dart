ee6db39d1fd5ad623d3f842d1c856d613bda6838
SandeepKomal <sandeepkomalp@gmail.com>
2023-04-12 14:52:35 +0530

    added profile page

---
+++ lib/pro.dart
@@ -0,0 +1,16 @@
+
+ 
+
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: Text(
+            'Profile',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      );
+}
