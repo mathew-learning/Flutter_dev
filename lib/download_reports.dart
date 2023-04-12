4f8b0a91e1abe68a36d17f5f63ab36a17fefadb6
SandeepKomal <sandeepkomalp@gmail.com>
2023-04-12 14:48:22 +0530

    added download reports page

---
+++ lib/download_reports.dart
@@ -0,0 +1,16 @@
+
+ 
+
+import 'package:flutter/material.dart';
+
+class DownloadReportsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: Text(
+            'Download Reports',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      );
+}
